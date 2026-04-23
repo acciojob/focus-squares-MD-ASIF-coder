393b534ac91226e52fc61dfb2e83dab97d44de02
MD ASIF <145357545+MD-ASIF-coder@users.noreply.github.com>
2026-04-23 11:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,11 @@
 const sqr = document.querySelectorAll('.square');
 
 sqr.addEventListener = ('hover', () => {
-	sqr.style.background-color: ''#6F4E37';
+	// sqr.style.background-color: ''#6F4E37';
+	
+	for (let i = 2; i <= 3; i++)
+		{
+			let box = document.getElementbyId(`#square${i}`);
+			box.style.background-color: '#6F4E37';
+		}
 })
